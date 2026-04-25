157751bd9a2b70337c134070b23e0b7699381165
Prabesh Bastola <prabeshbastola382@gmail.com>
2026-04-25 17:52:36 +0545

    form validation

--- accounts/views.py
+++ accounts/views.py
@@ -7,12 +7,27 @@ from django.contrib.auth.models import User
 def user_register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
+
         if form.is_valid():
-            user = User.objects.create_user(
-                username = form.cleaned_data['username'],
-                email = form.cleaned_data['email'],
-                password=form.cleaned_data['password']
-            )
+            if(form.cleaned_data['password'] != form.cleaned_data['confirm_password']):
+                form.add_error('confirm_password', 'Passwords do not match')
+                print("error",form.errors)
+                context_dict = {
+                    'form':form
+                }
+                return render(request, 'register.html',context_dict)
+
+            else:
+                user = User.objects.create_user(
+                    username = form.cleaned_data['username'],
+                    email = form.cleaned_data['email'],
+                    password=form.cleaned_data['password']
+                )
+                user.save()
+
+        else:
+            print("form is not valid", form.errors)
+
     form = RegistrationForm()
     context_dict = {
         'form':form
